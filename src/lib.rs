282347ed0ceb5d755bfbf5b5989dbaf725774966
swektr <samueljwaechter@gmail.com>
2023-08-22 22:27:05 -0400

    First implementation

---
+++ src/lib.rs
@@ -0,0 +1,196 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::io::Error as IOError;
+use std::env::var;
+use regex::Regex;
+use serde_derive::Deserialize;
+use clap::Parser;
+
+pub const DEFAULT_DEPTH: i32 = 2;
+
+trait PathExt {
+    fn has_extension(&self, e: &str) -> bool;
+}
+
+impl PathExt for PathBuf {
+    fn has_extension(&self, ext: &str) -> bool {
+        match self.extension() {
+            Some(x) if x == ext => true,
+            _ => false,
+        }
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ConfigWrap {
+    pub subdir: Option<PathBuf>,
+    pub depth: Option<i32>,
+}
+
+impl Into<Config> for ConfigWrap {
+    fn into(self) -> Config {
+        Config {
+            subdir: self.subdir.unwrap(),
+            depth: self.depth.unwrap(),
+        }
+    }
+}
+
+pub struct Config {
+    pub subdir: PathBuf,
+    pub depth: i32,
+
+}
+
+
+#[derive(Parser, Debug)]
+pub struct Cli {
+    /// Pattern to search
+    pub pattern: String,
+
+    /// Set the subtitle directory
+    #[clap(long,short,action)]
+    pub subdir: Option<PathBuf>,
+
+    #[clap(long,short,action)]
+    pub depth: Option<i32>,
+}
+
+
+pub fn get_sub_files(paths: &mut Vec<PathBuf>, dir: &Path, depth: i32) -> Result<(), IOError> {
+    if depth <= 0 {
+        return Ok(());
+    }
+
+    let items = fs::read_dir(dir)?;
+
+    for item in items {
+        let item = item?;
+        let path = item.path();
+
+        if path.is_file() && path.has_extension("ass") {
+            paths.push(path);
+        } else if path.is_dir() {
+            get_sub_files(paths, &path, depth - 1)?;
+        }
+    }
+
+    Ok(())
+}
+
+pub fn load_config(args: &Cli) -> Option<Config> {
+    let mut config = match read_config_file() {
+        Ok(config) => config,
+        Err(_) => {
+            ConfigWrap {subdir: None, depth: None}
+        }
+    };
+    // Cli args take precedence:
+    if args.subdir.is_some() {
+        config.subdir = args.subdir.clone();
+    }
+    if args.depth.is_some() {
+        config.depth = args.depth.clone();
+    }
+    
+    // "subdir" MUST be set one way or another:
+    if config.subdir.is_none() {
+        eprintln!("Must set subdir in config.toml OR using \"--subdir=\" with cli.");
+        return None;
+    }
+    if config.depth.is_none() {
+        config.depth = Some(DEFAULT_DEPTH);
+    }
+
+    // This appends a "/" to the end of the path if one is not there
+    config.subdir.as_mut().unwrap().push("");
+    
+    // return Some config with unwrapped members:
+    Some(config.into())
+}
+
+fn read_config_file() -> Result<ConfigWrap,Box<dyn std::error::Error>> {
+
+    let config_path = var("XDG_CONFIG_HOME")
+                    .or_else(|_| var("HOME")
+                        .map(|home| format!("{}/.config/jmks/config.toml",home))
+                    )?;        
+    
+    let contents = fs::read_to_string(&config_path)?;
+    let config = toml::from_str(&contents)?;
+    
+    Ok(config)
+}
+
+
+pub fn extract_sub_ass(line: &String) -> Option<(&str, &str, &str)>{
+    fn index_of_nth_tgt(s: &str, n: usize, tgt: char) -> Option<(usize,usize)> {
+        let mut count = 0;
+        for (char_idx, (byte_idx, char)) in s.char_indices().enumerate() {
+            if char == tgt {
+                count += 1;
+                if count == n {
+                    return Some((char_idx, byte_idx)) ;
+                }
+            }
+        }
+        None
+    }
+
+    let start = &line[12..22]; 
+    let end = &line[23..33];
+    let mut text = &line[34..];
+    
+    //Get index of Effect feild
+    let (char_idx, byte_idx) = index_of_nth_tgt(text, 5, ',').unwrap();
+    
+    // Make sure Effect feild is empty
+    // Ignore lines with effects because they're usually are not actual dialogue
+    text = match text.chars().nth(char_idx+1) {
+        Some(',') => &text[byte_idx+2..],
+        _ => return None,
+    };
+
+    // Text feilds that start with "{" usually are not dialogue either, so ignore
+    match text.chars().nth(0) {
+        Some('{') => None,
+        _ => Some((start,end,text)),
+    }
+}
+
+fn splice_out<'a>(line: &'a str, tgt: &str) -> Option<(&'a str, &'a str)> {
+    let start = match line.find(tgt) {
+        Some(start) => start,
+        None => return None,
+    };
+    let end = start+tgt.len();
+    Some((&line[..start], &line[end..]))
+}
+
+pub fn splice_out_all_and_replace_into<'a>(into: &mut String, line: &'a str, tgt_cut: &str, tgt_repl: char) {
+    into.clear();
+    let mut next = line;
+    while let Some((left, right)) = splice_out(next, tgt_cut) {
+        into.push_str(left);
+        into.push(tgt_repl);
+        next = right;
+    }
+    into.push_str(next);
+    
+}
+
+pub fn highlight_matches(formated_line: &mut String, text: &str, re: &Regex) {
+    let mut slice = text;
+    while let Some(m) = re.find(slice){
+        if m.start() == m.end() { 
+            break; 
+        }
+        formated_line.push_str(&slice[..m.start()]); // before match
+        for item in ["\x1b[1;31m", m.as_str(), "\x1b[0m"] {
+            formated_line.push_str(item);
+        }
+        slice = &slice[m.end()..];
+    }
+    formated_line.push_str(slice);
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use std::{process::ExitCode, 
+          path::PathBuf,
+          fs::File,
+          io::{BufReader, BufRead}};
+use clap::Parser;
+use regex::Regex;
+use jmks::*;
+
+fn main() -> ExitCode {
+    // Set up
+    let args = Cli::parse();
+
+    let config = match load_config(&args) {
+        Some(config) => config,
+        None => return ExitCode::FAILURE,
+    };
+
+    let re = Regex::new(&args.pattern).unwrap();
+    let subdir_length = config.subdir.as_os_str().to_str().unwrap().len();
+    
+    // Find subtitle files and sort
+    let mut paths: Vec<PathBuf> = Vec::new();
+    get_sub_files(&mut paths, &config.subdir, config.depth).unwrap();
+    paths.sort();
+
+    let mut formated_line = String::new();
+    let mut text = String::new();
+    for path in &paths {
+        let file = File::open(path).unwrap();
+        let reader = BufReader::new(file);
+
+        let path = path.as_os_str().to_str().unwrap();
+        let trunc_path_str = &path[subdir_length..];
+        
+        for line in reader.lines() {
+            let line = line.unwrap();
+            if  line.len() < 10 || &line.as_bytes()[..10] != b"Dialogue: " {
+                continue;
+            }
+            let (start,end,text_slice) = match extract_sub_ass(&line) {
+                Some(x) => x,
+                _ => continue,
+            };
+            // Slightly faster than str.replace()
+            splice_out_all_and_replace_into(&mut text, text_slice, r"\N", ' ');
+
+            if ! re.is_match(&text) {
+                continue; 
+            }
+            // Details about the line of text
+            for item in [trunc_path_str, ": ", start, " ", end, " => "] {
+                formated_line.push_str(item);
+            }
+            
+            highlight_matches(&mut formated_line, &text, &re);
+
+            println!("{formated_line}");
+            formated_line.clear(); 
+        }
+    }
+    ExitCode::SUCCESS
 }
